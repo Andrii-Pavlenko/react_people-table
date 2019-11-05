4d3e20eb89b17432f8afc17e493ccb7fcc3fd33f
Andrii <farikpite@gmail.com>
2019-11-05 22:22:03 +0200

    Updates

---
+++ precache-manifest.1d20d6794e0c0f011fcca64c67bb6ea4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ecb978c87e7ed120716f78f0bb618d",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "9d690d0b0a6506610ff4",
+    "url": "/react_people-table/static/css/main.4288bfd1.chunk.css"
+  },
+  {
+    "revision": "caf6b14346ab3802a7ee",
+    "url": "/react_people-table/static/js/2.15a4009c.chunk.js"
+  },
+  {
+    "revision": "9d690d0b0a6506610ff4",
+    "url": "/react_people-table/static/js/main.2231bb55.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
